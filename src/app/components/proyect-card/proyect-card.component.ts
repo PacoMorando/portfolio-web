4354fdd41af3ae6bab2304b181dbbb32a220bea7
PacoLapLinux <paco.morando@gmail.com>
2023-09-16 15:05:30 -0600

    Avances en proyectModal

--- src/app/components/proyect-card/proyect-card.component.ts
+++ src/app/components/proyect-card/proyect-card.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var window: any;
 
 @Component({
   selector: 'app-proyect-card',
   templateUrl: './proyect-card.component.html',
   styleUrls: ['./proyect-card.component.css']
 })
-export class ProyectCardComponent {
-  protected redirectToProject() {
-    console.log("CARD PICADA");
+export class ProyectCardComponent implements OnInit {
+  private proyectModal: any;
+  protected proyectName: string = "Nombre del proyecto";
+
+  ngOnInit(): void {
+    this.proyectModal = new window.bootstrap.Modal(document.getElementById("proyectModal"));
+  }
+
+  protected showProject(proyectName: string ) {
+    this.proyectName = proyectName;
+    this.proyectModal.show();
+    console.log("Card: " + this.proyectName + " picada");
   }
 }
