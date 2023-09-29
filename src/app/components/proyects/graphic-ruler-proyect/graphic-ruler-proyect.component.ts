260a1991fd70b086ea5e819ca76bcd6a84cae25a
NYP_PC <paco.morando@gmail.com>
2023-09-29 13:12:59 -0600

    Detalles de proyecto Graphic Ruler en español TERMINADO

--- src/app/components/proyects/graphic-ruler-proyect/graphic-ruler-proyect.component.ts
+++ src/app/components/proyects/graphic-ruler-proyect/graphic-ruler-proyect.component.ts
@@ -9,5 +9,21 @@ export class GraphicRulerProyectComponent {
   protected proyectName: string = "Graphic Ruler";
   protected releaseURL: string = "https://play.google.com/store/apps/details?id=com.sas.graphicruler";
   protected gitHubURL: string = "https://github.com/PacoMorando";//TODO FALTA ACTUALIZAR URL DEL REPOSITORIO
+  protected projectCode: string = `
+package com.sas.graphicruler.views;
+
+class Context {...
+
+  static float getDeviceHeight() {
+    return Context.getInstance().getResources().getDisplayMetrics().ydpi;
+
+    static int getTotalScreenHeightInInches() {
+      return (int) Math.ceil(Context.getInstance().getResources().getDisplayMetrics().heightPixels/Context.getDeviceHeight() + 1);
+      //"+1" is a factor because the getDisplayMetrics().heightPixels is not considering the height pixel from the UI bars
+  }
+  static float getRealTotalScreenHeightInInches() {
+      return Context.getInstance().getResources().getDisplayMetrics().heightPixels/Context.getDeviceHeight();
+  }
+}`;
 
 }
