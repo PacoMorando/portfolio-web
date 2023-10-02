279308a7e971415717c1244fe684abad5c7b69f1
NYP_PC <paco.morando@gmail.com>
2023-10-02 13:38:03 -0600

    Avance Listagram

--- src/app/components/proyects/master-mind-proyect/master-mind-proyect.component.ts
+++ src/app/components/proyects/master-mind-proyect/master-mind-proyect.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./master-mind-proyect.component.css']
 })
 export class MasterMindProyectComponent {
-
+  protected proyectName: string = "Master Mind";
+  protected releaseURL: string = "https://play.google.com/store/apps/developer?id=Saturn+Apple+Studios";
+  protected gitHubURL: string = "https://github.com/PacoMorando";
 }
