922125c907fca3b6d26fc0b460550149e38f2685
NYP_PC <paco.morando@gmail.com>
2023-10-28 08:39:24 -0600

    Update Resume

--- src/app/components/proyects/master-mind-proyect/master-mind-proyect.component.ts
+++ src/app/components/proyects/master-mind-proyect/master-mind-proyect.component.ts
@@ -9,6 +9,7 @@ export class MasterMindProyectComponent {
   protected proyectName: string = "Master Mind";
   protected releaseURL: string = "https://play.google.com/store/apps/developer?id=Saturn+Apple+Studios";
   protected gitHubURL: string = "https://github.com/PacoMorando/multiplatform-master-mind";
+  protected gitHubCoreURL: string = "https://github.com/PacoMorando/master-mind-core";
   protected gitHubConsoleURL: string = "https://github.com/PacoMorando/master-mind-console";
   protected gitHubAndroidURL: string = "https://github.com/PacoMorando/master-mind-android";
   protected gitHubAPIURL: string = "https://github.com/PacoMorando/master-mind-api";
